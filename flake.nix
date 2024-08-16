56f94b71475ff1053f8c1b3d07db5e8327f3f4d0
IogaMaster <iogamastercode@gmail.com>
2024-08-16 13:16:51 -0600

    :tada: initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+  in rec {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          alejandra
+        ];
+      };
+    });
+
+    packages = forAllSystems (pkgs: {
+      mcman = pkgs.callPackage ./pkgs/mcman {};
+      playit = pkgs.callPackage ./pkgs/playit {};
+    });
+
+    nixosModules.flux = ./modules/flux;
+    nixosModules.default = nixosModules.flux;
+
+    overlays.default = final: prev: {
+      mcman = final.callPackage ./pkgs/mcman {};
+      playit = final.callPackage ./pkgs/playit {};
+    };
+  };
+}
